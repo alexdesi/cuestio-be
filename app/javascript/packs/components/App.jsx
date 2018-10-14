fdf5d54ba7c4c748a6a10d252ba33464cc5bd438
Alexdesi <alexdesi@gmail.com>
2018-10-14 08:26:50 +0100

    Adds Bootstrap

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -7,13 +7,13 @@ import { QuizImport } from './QuizImport.jsx'
 
 class App extends Component {
   render() {
+    console.log('APP started!!!!')
     return (
       <BrowserRouter>
         <div className="App">
           <header className="App-header">
             <h1 className="App-title">Quiz</h1>
           </header>
-
 
           <Switch>
             <Route exact path="/quizzes" component={ QuizzesMenu }/>
